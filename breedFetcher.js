7093443653bd0561dc606dcce302d6b4af33e040
Ayo-Show <sowunmi30@gmail.com>
2021-11-04 16:04:53 -0400

    Breed details fetcher

---
+++ breedFetcher.js
@@ -0,0 +1,19 @@
+const request = require('request');
+
+const breedName = process.argv[2];
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(`Request failed: ${error}`);
+  }
+
+  const data = JSON.parse(body);
+
+  const breed = data[0];
+  if (breed) {
+    console.log(breed.description);
+  } else {
+    console.log(`Failed to find breed ${breedName}`);
+  }
+});
